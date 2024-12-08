32b476eed3a9dd6e772e38c2822ee051d222cb16
Mateja Rilak <matejarilak@Matejas-MacBook-Pro.local>
2024-12-08 11:30:06 +0100

    Exception handler initialized with a base exception and api error

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from shared.exceptions import ApiError, BaseHTTPException, ValidationFailed
 
 app = FastAPI()
 
+@app.exception_handler(BaseHTTPException)
+async def handle_all_exceptions(request: Request, exc: BaseHTTPException):
+    return JSONResponse(
+        content=ApiError(message=exc.detail, status_code=exc.status_code, path=request.url.path).model_dump(),
+        status_code=exc.status_code,
+    )
 
 @app.get("/")
 async def root():
---
+++ backend/shared/exceptions.py
@@ -0,0 +1,23 @@
+import datetime
+from typing import Callable
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+class ApiError(BaseModel):
+    message: str
+    status_code: int
+    path: str 
+    time: str = str(datetime.datetime.now())
+
+    
+class BaseHTTPException(HTTPException):
+    def __init__(self, status_code, detail = None, headers = None):
+        super().__init__(status_code, detail, headers)
+
+
+class ValidationFailed(BaseHTTPException):
+    def __init__(self, status_code, detail=None, headers=None):
+        super().__init__(status_code, detail, headers)
+
+
